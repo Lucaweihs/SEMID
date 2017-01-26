25cb91303f2362db590f074c297f5bc4624e3a4c
Luca Weihs <astronomicalcuriosity@gmail.com>
2017-01-26 10:05:04 -0800

    Improving the plotting of mixed graphs.

--- R/plotting.R
+++ R/plotting.R
@@ -8,29 +8,31 @@
 #'
 #' @export
 plotMixedGraph <- function(L, O, main = "", vertexLabels = 1:nrow(L)) {
-  R.utils::withSeed({
-  m = ncol(L)
-  dirGraph = igraph::graph.adjacency(L)
-  igraph::V(dirGraph)$name = vertexLabels
-  biGraph = igraph::graph.adjacency(O, mode = "undirected")
-  igraph::V(biGraph)$name = vertexLabels
-
-  layout = igraph::layout_in_circle(dirGraph)
-
-  plot(dirGraph, edge.color = "blue", layout = layout, vertex.size = 30,
-       vertex.color = "lightgrey", main = main)
-
-  biEdgeList = igraph::get.edgelist(biGraph)
-  curvedOrNot = logical(nrow(biEdgeList))
-  if (nrow(biEdgeList) != 0) {
-    for (i in 1:nrow(biEdgeList)) {
-      v1 = biEdgeList[i,1]
-      v2 = biEdgeList[i,2]
-      curvedOrNot[i] = (L[v1,v2] || L[v2,v1])
-    }
+  if (nrow(L) == 0) {
+    stop("Can only plot graphs with >= 1 vertex")
   }
-  plot(biGraph, add = T, edge.color = "red", layout = layout,
-       edge.curved = curvedOrNot, edge.arrow.mode = 3, vertex.size = 30,
-       vertex.color = "lightgrey")
+  R.utils::withSeed({
+    dirGraph = igraph::graph.adjacency(L)
+    igraph::V(dirGraph)$name = vertexLabels
+    biGraph = igraph::graph.adjacency(O, mode = "undirected")
+    igraph::V(biGraph)$name = vertexLabels
+
+    layout = igraph::layout_in_circle(dirGraph)
+
+    dirEdgeList = igraph::get.edgelist(dirGraph)
+    dirCurveMat = (L * t(L)) != 0
+    curvedOrNot = dirCurveMat[dirEdgeList]
+
+    plot(dirGraph, edge.color = "blue", layout = layout,
+         edge.curved = curvedOrNot, vertex.size = 30,
+         vertex.color = "lightgrey", main = main)
+
+    biEdgeList = igraph::get.edgelist(biGraph)
+    biCurveMat = ((L + t(L)) != 0) * O * (1 - dirCurveMat)
+    curvedOrNot = biCurveMat[biEdgeList] != 0
+
+    plot(biGraph, add = T, edge.color = "red", layout = layout,
+         edge.curved = curvedOrNot, edge.arrow.mode = 3, vertex.size = 30,
+         vertex.color = "lightgrey")
   }, 123)
 }
